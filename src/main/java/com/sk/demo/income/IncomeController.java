a05392070717d054122b90b4e4821b9188c038b5
Santhosh kumar <santhosh@Santhoshs-MacBook-Air.local>
2026-02-12 22:33:18 +0530

    adding loggers

--- src/main/java/com/sk/demo/income/IncomeController.java
+++ src/main/java/com/sk/demo/income/IncomeController.java
@@ -1,6 +1,10 @@
 package com.sk.demo.income;
 
+import com.sk.demo.common.WealthConstants;
 import com.sk.demo.income.dto.IncomeRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/income")
+@Slf4j
 public class IncomeController {
+    private static final Logger logger = LoggerFactory.getLogger(WealthConstants.class);
     @Autowired
     IncomeService incomeService;
 
     @PostMapping("/summary")
     public ResponseEntity<Object> getSummary(@RequestBody IncomeRequest request){
+        logger.info("Received income summary request: {}", request);
         var response=incomeService.calculateIncomeSummary(request);
         return ResponseEntity.ok(response);
 
